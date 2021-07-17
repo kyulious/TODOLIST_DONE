451bf514ab8ddf184f8637efa223512e958231e0
kyulious <kyulikim1@gmail.com>
2021-07-17 11:42:03 +0900

    commit2

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const down = document.getElementById("downa");
 const finish = document.getElementById("finish");
 const items = document.getElementById("items");
 const finishitem = document.getElementById("finishitem");
+const menu2 = document.getElementById("menu2");
 
-//시계 구현 
+//시계  
 const timer = () => {
     const clock = document.querySelector('.clock');
     const today = new Date();
@@ -63,7 +64,8 @@ ck.addEventListener("click", function(){
         })
 
         //별이미지
-        const img = document.createElement('img');
+        var img = document.createElement('img');
+        img.id="starem";
         img.src = "./image/starem.png";
         item.appendChild(img);
         img.style.width="20px";
@@ -73,35 +75,84 @@ ck.addEventListener("click", function(){
         //별 클릭 시 노란색으로 -> 중요로 이동해야함
         img.addEventListener("click", function(e){
             //이벤트 버블링 방지
-            e.stopPropagation();
-            const newimg = document.createElement('img');
+            //e.stopPropagation();
+            var newimg = document.createElement('img');
+            img.id="starfu";
             img.src = "./image/starfu.png";
-            img.appendChild(newimg);
+
+
+            if(img.src="./image/starem.png"){
+                img.id="starfu";
+                img.src="./image/starfu.png";
+            }
+            else {
+                img.id.replace=("starem");
+                img.src.replace=("./image/starem.png");
+            }
+            // img.src = "./image/starfu.png";
+            // img.appendChild(newimg);
         })
     }
     
 });
 
-//엔터키 텍스트 추가 -> 고쳐야함 (왜 동작을 안하지?!?!?!?)
-text.addEventListener("keyup", function(){
-
-    if(keyCode === 13 && text.value != ""){
-        item.style.display="inline";
+//엔터키 텍스트 추가 
+text.addEventListener("keyup", function(e){
+    e.preventDefault();
+    if(e.key === 'Enter' && text.value != ""){
+        items.style.display="inline";
         const list2 = document.createElement('ul');
         list2.innerText = text.value;
         items.appendChild(list2);
         ck.style.display="inline";
         text.value = "";
-        item.style.fontSize="20px";
-        item.style.marginLeft="20px";
-        item.style.marginTop="10px";
+        items.style.fontSize="20px";
+        items.style.marginLeft="20px";
+        items.style.marginTop="10px";
         list2.style.borderBottom="1px solid rgb(172, 172, 172)"
         list2.style.width="700px"
         list2.style.paddingTop="10px";
         list2.addEventListener('click', function(){
         list2.style.textDecoration = "line-through";
         })
+
+         //텍스트 클릭 시 줄을 그어줌
+         item.addEventListener('click', function(e){
+            e.stopPropagation();
+            list2.style.textDecoration = "line-through";
+            })
+    
+            //별이미지
+            var img = document.createElement('img');
+            img.id="starem";
+            img.src = "./image/starem.png";
+            list2.appendChild(img);
+            img.style.width="20px";
+            img.style.height="20px";
+            img.style.float="right";
+            img.style.cursor="pointer";
+            //별 클릭 시 노란색으로 -> 중요로 이동해야함????이건 우째하지
+            img.addEventListener("click", function(e){
+                //이벤트 버블링 방지
+                e.stopPropagation();
+                //e.target.src="./image/starfu.png";
+                // const newimg = document.createElement('img');
+                // newimg.id="starfu";
+                // img.appendChild(newimg);
+                
+            })
+            
     }
 });
 
-//더블 클릭시 완료됨으로 이동
+
+//별이랑 취소선 다시 누르면 없어지게 -> 구현해야함
+
+
+//더블 클릭시 완료됨으로 이동 -> 구현해야함
+
+//중요페이지에 아이템 옮기기 -> 구현해야함
+menu2.style.cursor="pointer";
+menu2.addEventListener('click', function(){
+    
+})
